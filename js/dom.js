8ccc9ac68d78c3d5f647b34c0b70879961b46f47
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-04-05 18:36:52 +0900

    javascriptテキスト

---
+++ js/dom.js
@@ -0,0 +1,28 @@
+console.log(document.head);
+
+console.log(document.body);
+
+console.log(window);
+
+console.log(document.getElementById('first-list'));
+
+console.log(document.getElementsByClassName('heading'));
+
+const headings = document.getElementsByClassName('heading');
+
+for(let i=0;i < headings.length;i++){
+  console.log(headings[i]);
+}
+
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#second-heading'));
+console.log(document.querySelector('.list'));
+
+console.log(document.querySelectorAll('.heading'));
+console.log(document.querySelectorAll('li'));
+
+const li = document.createElement('li');
+
+li.textContent = 'Javascriptで新しく作成したリスト3';
+
+document.querySelector('ul').appendChild(li);
